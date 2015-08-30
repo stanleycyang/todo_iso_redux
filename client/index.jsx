16a5a7068a0bf1f22fba3cd8d207ba7e4663c940
syang019 <stanley@stanleycyang.com>
2015-08-29 18:07:27 -0700

    Finished project

---
+++ client/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Router } from 'react-router';
+import { history } from 'react-router/lib/BrowserHistory';
+import { Provider } from 'react-redux';
+import { fromJS } from 'immutable';
+import * as reducers from 'reducers';
+import routes from 'routes';
+import promiseMiddleware from 'lib/promiseMiddleware';
+import immutifyState from 'lib/immutifyState';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+const initialState =immutifyState(window.__INITIAL_STATE__);
+
+const reducer = combineReducers(reducers);
+const store = applyMiddleware(promiseMiddleware)(createStore)(reducer, initialState);
+
+React.render(
+  <Provider store={store}>
+    {() =>
+      <Router children={routes} history={history} />
+    }
+  </Provider>,
+  document.getElementById('react-view')
+);
